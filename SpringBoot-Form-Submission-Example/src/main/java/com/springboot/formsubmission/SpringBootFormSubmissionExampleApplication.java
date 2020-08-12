5207b05ca61634753c0b40bb8ea3d4efcd7ca499
PrasannaThilakarathne <prasanna.srilal.7@gmail.com>
2020-08-12 13:20:10 +0530

    Initiate the example project for form handling

---
+++ SpringBoot-Form-Submission-Example/src/main/java/com/springboot/formsubmission/SpringBootFormSubmissionExampleApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.formsubmission;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootFormSubmissionExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootFormSubmissionExampleApplication.class, args);
+	}
+
+}
