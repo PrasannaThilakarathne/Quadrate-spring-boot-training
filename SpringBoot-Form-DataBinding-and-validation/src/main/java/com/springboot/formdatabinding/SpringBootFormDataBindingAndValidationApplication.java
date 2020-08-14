b5ec7a4b5fcaaeaed511c075cd99750f2f6b9821
PrasannaThilakarathne <prasanna.srilal.7@gmail.com>
2020-08-14 20:34:32 +0530

    Initiate the Project

---
+++ SpringBoot-Form-DataBinding-and-validation/src/main/java/com/springboot/formdatabinding/SpringBootFormDataBindingAndValidationApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.formdatabinding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootFormDataBindingAndValidationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootFormDataBindingAndValidationApplication.class, args);
+	}
+
+}
---
+++ SpringBoot-Form-DataBinding-and-validation/src/test/java/com/springboot/formdatabinding/SpringBootFormDataBindingAndValidationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.formdatabinding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootFormDataBindingAndValidationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
